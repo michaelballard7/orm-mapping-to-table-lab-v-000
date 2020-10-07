a85ad4942e967a8d17809c80bcd020bb8505fde1
Michael Ballard <ballard.michael9@gmail.com>
2020-10-07 23:39:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,9 @@ class Student
       # set sql statement
       sql = <<-SQL
         CREATE TABLE IF NOT EXISTS students (
-            id INTEGER primary key
-            name TEXT
-            grade INTEGER
+            id INTEGER primary key,
+            name TEXT,
+            grade INTEGER,
         )
         SQL
         DB[:conn].execute(sql)
