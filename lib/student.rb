ffa36142817ac6254c4c9683d7b377664a47a16a
Michael Ballard <ballard.michael9@gmail.com>
2020-10-07 23:51:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,12 @@ class Student
       # insert object into the db
       # use ??? then insert attributes
 
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
   end
 
 end
