3505dc7b27821cad5753848875dc8cee59bdbd06
Michael Ballard <ballard.michael9@gmail.com>
2020-10-07 23:36:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name, :id
+  attr_accessor :name, :id, :grade
   attr_reader :id
 
   def initialize name, grade
